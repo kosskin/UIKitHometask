ffeb1ab3e1a5c4d1aa991d4be0e2188318e73c6a
Valentin Koskin <>
2022-09-22 14:22:08 +0300

    init

---
+++ UIKitHometasks/UIKitHometasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitHometasks
+//
+//  Created by Валентин Коскин on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
