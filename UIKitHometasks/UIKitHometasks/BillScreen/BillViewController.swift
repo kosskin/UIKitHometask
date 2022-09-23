e7b6a0483ee38fff79e98f4d2f54109539143b71
Valentin Koskin <>
2022-09-23 09:44:43 +0300

    сделал 3 дз без сториборда

---
+++ UIKitHometasks/UIKitHometasks/BillScreen/BillViewController.swift
@@ -0,0 +1,87 @@
+//
+//  BillViewController.swift
+//  UIKitHometasks
+//
+//  Created by Валентин Коскин on 23.09.2022.
+//
+
+import UIKit
+
+/// Счёт - третий экран приложения
+class BillViewController: UIViewController {
+
+    let firstFoodTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.text = "Салат цезарь"
+        return textField
+    }()
+    
+    let secondFoodTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.text = "Салат греческий"
+        return textField
+    }()
+    
+    let firstFoodLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "400"
+        return label
+    }()
+    
+    let secondFoodLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "400"
+        return label
+    }()
+    
+    let resultButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Итого: 800 рублей", for: .normal)
+        button.backgroundColor = UIColor.systemPink
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        setConstraints()
+    }
+    
+    func configureUI() {
+        self.view.backgroundColor = .white
+        view.addSubview(firstFoodTextField)
+        view.addSubview(firstFoodLabel)
+        view.addSubview(secondFoodTextField)
+        view.addSubview(secondFoodLabel)
+        view.addSubview(resultButton)
+        title = "Чек"
+    }
+    
+    func setConstraints() {
+        firstFoodTextField.widthAnchor.constraint(equalToConstant: 335).isActive = true
+        firstFoodTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        firstFoodTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                                constant: 103).isActive = true
+        
+        firstFoodLabel.centerYAnchor.constraint(equalTo: firstFoodTextField.centerYAnchor).isActive = true
+        firstFoodLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+        
+        secondFoodTextField.widthAnchor.constraint(equalToConstant: 335).isActive = true
+        secondFoodTextField.topAnchor.constraint(equalTo: firstFoodTextField.bottomAnchor, constant: 52).isActive = true
+        secondFoodTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        secondFoodLabel.centerYAnchor.constraint(equalTo: secondFoodTextField.centerYAnchor).isActive = true
+        secondFoodLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+        
+        resultButton.topAnchor.constraint(equalTo: secondFoodTextField.bottomAnchor, constant: 263).isActive = true
+        resultButton.widthAnchor.constraint(equalToConstant: 190).isActive = true
+        resultButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        resultButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+    }
+}
---
+++ UIKitHometasks/UIKitHometasks/OrderScreen/OrderViewController.swift
@@ -0,0 +1,242 @@
+//
+//  OrderViewController.swift
+//  UIKitHometasks
+//
+//  Created by Валентин Коскин on 23.09.2022.
+//
+
+import UIKit
+
+/// класс второго экрана
+class OrderViewController: UIViewController {
+
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "ФИО"
+        label.textColor = .systemPink
+        return label
+    }()
+    
+    let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Введите ФИО"
+        textField.layer.borderWidth = 0.25
+        return textField
+    }()
+    
+    let amountGuestsLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Количество гостей"
+        label.textColor = .systemPink
+        return label
+    }()
+    
+    let amountGuestsTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Введите количество"
+        textField.layer.borderWidth = 0.25
+        return textField
+    }()
+    
+    let numberTableLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Номер стола"
+        label.textColor = .systemPink
+        return label
+    }()
+    
+    let numberTableTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Номер стола"
+        textField.layer.borderWidth = 0.25
+        return textField
+    }()
+    
+    let bookedTableLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Бронировали стол?"
+        label.textColor = .systemPink
+        return label
+    }()
+    
+    let bookedTableSwitch: UISwitch = {
+        let switchBook = UISwitch()
+        switchBook.translatesAutoresizingMaskIntoConstraints = false
+        switchBook.addTarget(self, action: #selector(switchTouch(sender:)), for: .valueChanged)
+        return switchBook
+    }()
+    
+    let payMoneyLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Предоплата?"
+        label.textColor = .systemPink
+        return label
+    }()
+
+    let payMoneySwitch: UISwitch = {
+        let switchMoney = UISwitch()
+        switchMoney.translatesAutoresizingMaskIntoConstraints = false
+        switchMoney.addTarget(self, action: #selector(switchTouch(sender:)), for: .valueChanged)
+        return switchMoney
+    }()
+    
+    let vipRoomLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "VIP комната?"
+        label.textColor = .systemPink
+        return label
+    }()
+    
+    let vipRoomSwitch: UISwitch = {
+        let switchRoom = UISwitch()
+        switchRoom.translatesAutoresizingMaskIntoConstraints = false
+        switchRoom.addTarget(self, action: #selector(switchTouch(sender:)), for: .valueChanged)
+        return switchRoom
+    }()
+    
+    let getBillButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Выставить счет", for: .normal)
+        button.backgroundColor = UIColor.systemPink
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(getBillButtonTouch), for: .touchUpInside)
+        return button
+    }()
+    
+    let information: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.alignment = .leading
+        return view
+    }()
+    
+    let moreInformation1: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .horizontal
+        view.alignment = .leading
+        return view
+    }()
+    
+    let moreInformation2: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .horizontal
+        view.alignment = .leading
+        return view
+    }()
+    
+    let moreInformation3: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .horizontal
+        view.alignment = .leading
+        return view
+    }()
+    
+    var getName: String? {
+        return nameTextField.text
+    }
+    
+    var getAmountGuests: Int? {
+        guard let guests = Int("\(String(describing: amountGuestsTextField.text))") else {
+            return nil
+        }
+        return guests
+    }
+    
+    var getNumberTable: Int? {
+        guard let tables = Int("\(String(describing: numberTableTextField.text))") else {
+            return nil
+        }
+        return tables
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+        setConstrains()
+    }
+    
+    func configUI() {
+        view.backgroundColor = UIColor.white
+        
+        information.addArrangedSubview(nameLabel)
+        information.addArrangedSubview(nameTextField)
+        information.addArrangedSubview(amountGuestsLabel)
+        information.addArrangedSubview(amountGuestsTextField)
+        information.addArrangedSubview(numberTableLabel)
+        information.addArrangedSubview(numberTableTextField)
+        
+        moreInformation1.addArrangedSubview(bookedTableLabel)
+        moreInformation1.addArrangedSubview(bookedTableSwitch)
+        moreInformation2.addArrangedSubview(payMoneyLabel)
+        moreInformation2.addArrangedSubview(payMoneySwitch)
+        moreInformation3.addArrangedSubview(vipRoomLabel)
+        moreInformation3.addArrangedSubview(vipRoomSwitch)
+        
+        view.addSubview(information)
+        view.addSubview(moreInformation1)
+        view.addSubview(moreInformation2)
+        view.addSubview(moreInformation3)
+        view.addSubview(getBillButton)
+        title = "Welcome in Cafe!"
+    }
+    
+    func setConstrains() {
+        information.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 140).isActive = true
+        information.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 59).isActive = true
+        information.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 59).isActive = true
+        
+        moreInformation1.topAnchor.constraint(equalTo: information.bottomAnchor, constant: 104.5).isActive = true
+        moreInformation1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 59).isActive = true
+        
+        moreInformation2.topAnchor.constraint(equalTo: moreInformation1.bottomAnchor, constant: 50).isActive = true
+        moreInformation2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 59).isActive = true
+
+        moreInformation3.topAnchor.constraint(equalTo: moreInformation2.bottomAnchor, constant: 50).isActive = true
+        moreInformation3.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 59).isActive = true
+
+        getBillButton.topAnchor.constraint(equalTo: moreInformation3.bottomAnchor, constant: 92).isActive = true
+        getBillButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        getBillButton.widthAnchor.constraint(equalToConstant: 297).isActive = true
+
+        bookedTableSwitch.leadingAnchor.constraint(equalTo: moreInformation1.leadingAnchor,
+                                                   constant: 240).isActive = true
+        payMoneySwitch.leadingAnchor.constraint(equalTo: moreInformation2.leadingAnchor, constant: 240).isActive = true
+        vipRoomSwitch.leadingAnchor.constraint(equalTo: moreInformation3.leadingAnchor, constant: 240).isActive = true
+
+    }
+    
+    @objc func getBillButtonTouch() {
+        let nextVC = BillViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+        }
+    
+    @objc func switchTouch(sender: UISwitch) {
+        if sender.isOn {
+            sender.isOn = false
+        } else {
+            sender.isOn = true
+        }
+    }
+    
+    func showAlert(message: String) {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Хорошо", style: .default, handler: nil)
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+}
