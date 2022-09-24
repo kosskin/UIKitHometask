ca3ad6c92459de7db7dd154e58609522da2de386
Valentin Koskin <>
2022-09-24 20:23:03 +0300

    сделал 1 и почти полностью 2 экран

---
+++ UIKitHometasks/UIKitHometasks/BirthdayListViewController.swift
@@ -0,0 +1,144 @@
+//
+//  BirthdayListViewController.swift
+//  UIKitHometasks
+//
+//  Created by Валентин Коскин on 24.09.2022.
+//
+
+import UIKit
+
+///BirthdayListViewController - отвечает за список людей с их др
+class BirthdayListViewController: UIViewController {
+    
+    private let jeremyNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.frame = CGRect(x: 82, y: 127, width: 61, height: 21)
+        label.text = "Jeremy"
+        return label
+    }()
+    
+    private let jeremyDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.frame = CGRect(x: 82, y: 156, width: 234, height: 21)
+        label.text = "10 марта, в среду исполнится 25 лет"
+        label.textColor = .gray
+        return label
+    }()
+    
+    private let jeremyDaysLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.frame = CGRect(x: 324, y: 140, width: 70, height: 21)
+        label.text = "18 дней"
+        label.textColor = .gray
+        return label
+    }()
+    
+    private let jeremyImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person.circle")
+        imageView.frame = CGRect(x: 8, y: 118, width: 66, height: 66)
+        imageView.tintColor = .gray
+        return imageView
+    }()
+    
+    private let mariaNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.frame = CGRect(x: 82, y: 195, width: 61, height: 21)
+        label.text = "Maria Lui"
+        return label
+    }()
+    
+    private let mariaDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.frame = CGRect(x: 82, y: 224, width: 234, height: 21)
+        label.text = "30 марта, в четверг исполнится 25 лет"
+        label.textColor = .gray
+        return label
+    }()
+    
+    private let mariaDaysLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.frame = CGRect(x: 324, y: 208, width: 70, height: 21)
+        label.text = "28 дней"
+        label.textColor = .gray
+        return label
+    }()
+    
+    private let mariaImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person.circle")
+        imageView.frame = CGRect(x: 8, y: 186, width: 66, height: 66)
+        imageView.tintColor = .gray
+        return imageView
+    }()
+    
+    private let jonyNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.frame = CGRect(x: 82, y: 266, width: 61, height: 21)
+        label.text = "Jony Stark"
+        return label
+    }()
+    
+    private let jonyDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.frame = CGRect(x: 82, y: 294, width: 234, height: 21)
+        label.text = "20 апреля, в субботу исполнится 25 лет"
+        label.textColor = .gray
+        return label
+    }()
+    
+    private let jonyDaysLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.frame = CGRect(x: 324, y: 278, width: 70, height: 21)
+        label.text = "53 дней"
+        label.textColor = .gray
+        return label
+    }()
+    
+    private let jonyImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person.circle")
+        imageView.frame = CGRect(x: 8, y: 256, width: 66, height: 66)
+        imageView.tintColor = .gray
+        return imageView
+    }()
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configuUI()
+
+    }
+    
+    func configuUI() {
+        view.backgroundColor = .white
+        view.addSubview(jeremyNameLabel)
+        view.addSubview(jeremyDescriptionLabel)
+        view.addSubview(jeremyDaysLabel)
+        view.addSubview(jeremyImage)
+        view.addSubview(mariaNameLabel)
+        view.addSubview(mariaDescriptionLabel)
+        view.addSubview(mariaDaysLabel)
+        view.addSubview(mariaImage)
+        view.addSubview(jonyNameLabel)
+        view.addSubview(jonyDescriptionLabel)
+        view.addSubview(jonyDaysLabel)
+        view.addSubview(jonyImage)
+    }
+    
+//    func plusButtonAdd() {
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(<#T##@objc method#>))
+//    }
+//
+    @objc func pushNewView() {
+        
+    }
+}
---
+++ UIKitHometasks/UIKitHometasks/SignInViewController.swift
@@ -0,0 +1,133 @@
+//
+//  ViewController.swift
+//  UIKitHometasks
+//
+//  Created by Валентин Коскин on 22.09.2022.
+//
+import UIKit
+
+/// SignInViewController с экраном входа
+class SignInViewController: UIViewController {
+    
+    private let birthdayReminderLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Birthday Reminder"
+        label.textColor = .systemBlue
+        label.textAlignment = .center
+        label.layer.borderWidth = 1.5
+        label.frame = CGRect(x: 112, y: 100, width: 190, height: 77)
+        return label
+    }()
+    
+    private let signInLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sign In"
+        label.font = UIFont(name: "Apple Symbols", size: 34)
+        label.frame = CGRect(x: 44, y: 216, width: 110, height: 48)
+        return label
+    }()
+    
+    private let emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Email"
+        label.frame = CGRect(x: 44, y: 278, width: 42, height: 21)
+        label.textColor = .systemBlue
+        return label
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.frame = CGRect(x: 44, y: 313, width: 283, height: 34)
+        textField.placeholder = "examle@email.com"
+        let bottom = CALayer()
+        bottom.frame = CGRect(x: 0, y: textField.frame.height - 1, width: textField.frame.width, height: 1.0)
+        textField.layer.addSublayer(bottom)
+        bottom.backgroundColor = UIColor.gray.cgColor
+        return textField
+    }()
+
+    private let passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Password"
+        label.frame = CGRect(x: 44, y: 386, width: 84, height: 21)
+        label.textColor = .systemBlue
+        return label
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.frame = CGRect(x: 44, y: 422, width: 283, height: 34)
+        textField.placeholder = "password"
+        textField.isSecureTextEntry = true
+        let bottom = CALayer()
+        bottom.frame = CGRect(x: 0, y: textField.frame.height - 1, width: textField.frame.width, height: 1.0)
+        textField.layer.addSublayer(bottom)
+        bottom.backgroundColor = UIColor.gray.cgColor
+        return textField
+    }()
+  
+    private let eyeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "eye"), for: .normal)
+        button.frame = CGRect(x: 278, y: 434, width: 34, height: 18)
+        button.tintColor = .lightGray
+        button.addTarget(self, action: #selector(eyeButtonAction(sender:)), for: .touchUpInside)
+        return button
+    }()
+    private let faceIDLabel: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 122, y: 517, width: 148, height: 21)
+        label.text = "Вход по Face ID"
+        return label
+    }()
+    
+    private let faceIDSwitch: UISwitch = {
+        let switcher = UISwitch()
+        switcher.frame = CGRect(x: 278, y: 512, width: 0, height: 0)
+        switcher.isOn = true
+        return switcher
+    }()
+    
+    private let enterButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 42, y: 641, width: 330, height: 47)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Войти", for: .normal)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(enterButtonAction(sender:)), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+    }
+    
+    func configUI() {
+        view.backgroundColor = .white
+        view.addSubview(birthdayReminderLabel)
+        view.addSubview(signInLabel)
+        view.addSubview(emailLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordLabel)
+        view.addSubview(passwordTextField)
+        view.addSubview(faceIDLabel)
+        view.addSubview(faceIDSwitch)
+        view.addSubview(enterButton)
+        view.addSubview(eyeButton)
+    }
+    
+    @objc func eyeButtonAction(sender: UIButton) {
+        if passwordTextField.isSecureTextEntry == true {
+            passwordTextField.isSecureTextEntry = false
+        } else {
+            passwordTextField.isSecureTextEntry = true
+        }
+    }
+    
+    @objc func enterButtonAction(sender: UIButton) {
+        let birthdayListViewController = BirthdayListViewController()
+        navigationController?.pushViewController(birthdayListViewController, animated: true)
+    }
+}
